979be1058230e845c69ab5fc9a7d4b1d400657c8
Pratiksha-pari <parihar.pratiksh59@gmail.com>
2026-08-29 02:30:26 +0530

    ex

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-console.log("this is just for experiment");
-let x= 78; 
-let y = 30;
-let res = x + y;
-console.log(res);
-console.log(x-y);
-console.log(x * y)
+console.log("Hi. i am from child branch");
+
   
-console.log("***********************************************");
+console.log("**all the parent branch code automatically comes to child branch***");
